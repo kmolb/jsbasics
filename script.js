ba8c81cb491816ad4c65942ca26e6f5a58463ef8
Kamil Olber <olb.kamil@gmail.com>
2018-09-22 10:49:15 +0200

    types convertion

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-// alert("Hello World");
+var num = 1;
+var stringNum = "2";
+var stringNotNumber = "Hello world"
+console.log(Number(stringNotNumber)); //Nan
 
-// var testType = "hello types";
-// console.log('type of:',typeof testType);
- 
-var a = 1;
-var b = 2;
+var constructorNum = Number(stringNum);
 
-console.log('Defination:', a, b);
+console.log(num + stringNum); //12
+console.log(num + Number(stringNum)); //3
+console.log(num + constructor);
 
-var c= a + b;
-console.log('Assigment:', c);
+var array1 = [ ];
+var array2 = [ ];
+
+console.log(array1 + array2);
